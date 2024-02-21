aad88c48c90cf66d05a4d84ccfefc11e63afa386
JackMeds <1530043661@qq.com>
2024-02-21 19:31:01 +0800

    更新

--- src/views/NTS/NTS_Create.vue
+++ src/views/NTS/NTS_Create.vue
@@ -5,18 +5,18 @@
         </template>
     </van-nav-bar>
     <div class="Menu_Container">
-        <div class="top">
+        <div class="top" @click="openNovelList()">
             <p>{{ novel.name ? novel.name : "请选择小说" }}</p>
             <p>{{ novel.chapter ? novel.chapter : "请选择章节" }}</p>
         </div>
         <div class="bottom">
             <div class="left" @click="showDefault()">
                 <p>全文声线</p>
-                <p>{{ defaultVoice ? defaultVoice : "以进行AI识别" }}</p>
+                <p>{{ defaultVoice ? defaultVoice : "默认设置" }}</p>
             </div>
             <div class="right">
                 <div class="top" @click="inputKey()">
-                    <p>{{ APIKey ? APIKey : "输入OpenAI API key" }}</p>
+                    <p class="ellipsis-text">{{ APIKey ? APIKey : "输入OpenAI API key" }}</p>
                     <p>{{ APIKey ? "欢迎使用" : "以进行AI识别" }}</p>
                 </div>
                 <div class="bottom" @click="AIidentify()">
@@ -28,16 +28,19 @@
     </div>
     <van-tabs v-model:active="tabs_active" sticky swipeable class="tabs">
         <van-tab title="原文预览" class="novel">
-            <p v-if="normalNovel">{{ novel.content }}</p>
-            <van-highlight :keywords="highlightContent" :source-string="novel.content" highlight-class="custom-class" />
+            <div class="noRole" v-if="!showNovel">
+                <p>请先选择小说</p>
+            </div>
+            <p v-if="normalNovel" class="pre-line">{{ novel.content ? novel.content : "" }}</p>
+            <van-highlight class="pre-line" :keywords="highlightContent" :source-string="novel.content" highlight-class="custom-class" />
         </van-tab>
         <van-tab title="角色声线选择（AI）" class="roles">
             <div class="noRole" v-if="!showRoleList">
                 <p>请先进行AI识别，才能分析出小说角色</p>
             </div>
             <van-cell-group v-if="showRoleList">
-                <van-cell is-link v-for="(item, index) in PreProcess.Roles" :value="item.voiceName" size="large" style="padding: 15px 20px;"
-                    @click="setVoice(item.RoleID)">
+                <van-cell is-link v-for="(item, index) in PreProcess.Roles" :value="item.voiceName" size="large"
+                    style="padding: 15px 20px;" @click="setVoice(item.RoleID)">
                     <template #title>
                         <span class="custom-title">{{ item.RoleName }}</span>
                         <van-tag type="primary"></van-tag>
@@ -48,13 +51,17 @@
         </van-tab>
     </van-tabs>
     <div class="Bottom_Bar">
-        <div class="left">
+        <div class="left" @click="doNTS()">
             <p>开始制作</p>
         </div>
-        <div class="right">
+        <div class="right" @click="reset()">
             <p>重置</p>
         </div>
     </div>
+    <van-popup v-model:show="showNovelList" overlay position="bottom" round class="DefaultVoiceList">
+        <van-picker title="选择小说" :columns="NovelColumns" @confirm="onConfirmNovel" @cancel="onCancelNovelList"
+            style="background-color: rgb(255, 255, 255);" />
+    </van-popup>
     <van-dialog v-model:show="showInputKey" title="请输入OpenAI API key" show-cancel-button>
         <van-cell-group inset style="margin: 10px 20px;">
             <van-field v-model="APIKey" label="key" placeholder="请输入OpenAI API key" />
@@ -65,8 +72,8 @@
             @cancel="onCancelDefaultVoice" style="background-color: rgb(255, 255, 255);" />
     </van-popup>
     <van-popup v-model:show="showVoice" overlay position="bottom" round class="DefaultVoiceList">
-        <van-picker title="选择角色声线" :columns="VoiceColumns" @confirm="onConfirmVoice"
-            @cancel="onCancelVoice" style="background-color: rgb(255, 255, 255);" />
+        <van-picker title="选择角色声线" :columns="VoiceColumns" @confirm="onConfirmVoice" @cancel="onCancelVoice"
+            style="background-color: rgb(255, 255, 255);" />
     </van-popup>
     <div v-show="showLoading" class="loading">
         <div class="littleBox"><van-loading size="24px" color="#000" vertical>加载中...</van-loading></div>
@@ -74,39 +81,68 @@
 </template>
 <script setup>
 import { getCurrentInstance, ref } from 'vue';
+import { showConfirmDialog } from 'vant';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const instance = getCurrentInstance();
 const axios = instance.appContext.config.globalProperties.$axios;
 
+//打开小说列表
+const showNovelList = ref(false);
+const showNovel = ref(false);
+const NovelColumns = [];
+const onConfirmNovel = ({ selectedValues }) => {
+    showNovelList.value = false;
+    normalNovel.value = true;
+    showRoleList.value = false;
+    defaultVoice.value = '';
+    axios.get('/NTS/getnovel', {
+        params: {
+            novelName: selectedValues[0]
+        }
+    }).then((response) => {
+        console.log(response.data.data);
+        novel.value = {
+            novelID: 0,
+            chapterID: 0,
+            name: selectedValues[0],
+            chapter: "全一章",
+            content: response.data.data.data
+        };
+        showNovel.value = true;
+        console.log(novel.value);
+    }).catch((error) => {
+        console.log(error);
+    });
+};
+const onCancelNovelList = () => {
+    showNovelList.value = false;
+};
+const openNovelList = () => {
+    axios.get('/NTS/getnovellist').then((response) => {
+        console.log(response.data);
+        response.data.data.data.forEach((elm) => {
+            NovelColumns.push({
+                text: elm,
+                value: elm,
+            });
+        });
+        showNovelList.value = true;
+    }).catch((error) => {
+        console.log(error);
+    });
+};
+
 //预处理数据
 const PreProcess = ref({
-    "Roles": [
-        {
-            "RoleID": "1",
-            "RoleName": "秦诚"
-        },
-        {
-            "RoleID": "2",
-            "RoleName": "王煊"
-        }
-    ],
-    "content": [
-        {
-            "RoleID": "1",
-            "RoleName": "秦诚",
-            "text": "这种旧时代的东西，你真的练成了？"
-        },
-        {
-            "RoleID": "2",
-            "RoleName": "王煊",
-            "text": "按照就旧时代的说法，我这些不算什么。"
-        },
-        {
-            "RoleID": "1",
-            "RoleName": "秦诚",
-            "text": "你别拿过去来比较，我压根就没相信过，事实上，那些记载许多都早已被证伪。"
-        }
-    ]
+    novelID: 0,
+    chapterID: 0,
+    novelName: '',
+    chapterName: '',
+    content: [],
+    Roles: []
 });
 
 // 声音列表
@@ -187,11 +223,13 @@ var SoundList = [
 
 const novel = ref(
     {
-        name: '神器的小说',
-        chapter: '第一章',
-        content: "“这种旧时代的东西，你真的练成了？”秦诚冲了过去，看着粗大树干上的清晰手印，惊疑不定。\n\n旧时代散落在故纸堆里的术，从体术到冥想，再到采气术、内养法等，各种记载相当的零散。\n\n现如今这些被称作散术，也有很多人称它们为旧术。\n\n这些年，秦诚虽然一直在接触，但是内心深处却始终有所怀疑，真的能练成吗？\n\n王煊的表现让他大受触动，血肉手掌居然可以在粗大而坚硬的树干上留下清晰的手印，旧术……真的有路可走？！\n\n“按照就旧时代的说法，我这些不算什么。”王煊摇头，人很英挺，在皎洁月辉下像是附着淡淡的白雾。\n\n秦诚叹气：“你别拿过去来比较，我压根就没相信过，事实上，那些记载许多都早已被证伪。”\n\n秦诚也在练旧术，他深知有多难，仅一些冥想术足以让人精神分裂。\n\n在与新星有密切联系的今天，深空中科技灿烂，有几人还会相信这些？未练心中就已动摇。\n\n然而，正是在这种背景下，王煊、秦诚等人初进大学时，就有人找上他们，问他们是否愿意进行与旧术有关的研究。\n\n王煊以前就接触过散术，顿时很感兴趣，当场便答应了。\n\n至于秦诚，根本就不信这些，只是后来听到这个研究与某些财阀有关，据悉投入资金很大，他才惊异，抱着试试看的态度加入进来。\n\n早期，他们一边在自己所在的院系学原本的专业课，一边利用课余时间去参与所谓的“旧术研究”。\n\n他们发现，所谓的研究，其实主要还是让他们自己来练。\n\n并且开始有专人为他们准备特殊的食物，一切都是为了更好的练成某些散术。\n\n他们越发觉得，这项“研究”很不简单，教他们散术的两位老教授，竟然是从新星调过来的。\n\n很快，有些人就坚持不住了，主动退出，因为练旧术很辛苦，颇为枯燥。\n\n最主要的是，在如今这个年代，人们对旧时代故纸堆里的术持怀疑态度。\n\n不过，退出的人虽然不少，但新加入的却更多。\n\n因为背后有财阀支持，这项“研究”投入的资金很大，开始加大力量挑选更多合适的人参与进来。\n\n不仅是在这座城市，旧土各地都在进行，主要是以高校的学生为主，为了调动积极性，有不菲的薪酬激励。\n\n当初报名的人非常多，但是当大规模选拔后，又经过大半年的淘汰，最终只留下五十人左右。\n\n这些人大多数都转学了，集中到王煊与秦诚所在的这所大学，一边继续原本的专业，一边组成一个“旧术实验班”。\n\n并不是所有人都如同王煊般，最初是因为兴趣加入。\n\n转学来的人都已经被承诺，毕业后，最起码会保证他们一份得体且薪资很高的工作。\n\n至此，所有人都意识到，这项“研究”很不同寻常，不然为何要花费这样大的力气进行？\n\n实验班的“旧术教材”都有些来头，有博物馆的孤本，也有财阀的稀珍收藏，还有某些宗教的经文。\n\n比如某一内养法，在旧时代就很出名，但最终却失传了。\n\n数年前，有人在一座坟墓中发现，让它重见天日，如今被整理出来，成为实验班的研读物之一。\n\n此外，旧术实验班所有学生的饮食至此都有专人准备，非常讲究。\n\n随着了解，许多人深感惊异，部分食材竟极其珍贵与稀有，在旧土很难获取，是从深空运过来的。\n\n教他们采气术的老教授曾感慨，这些食材纵使在新星也都昂贵的吓死人，更何况是自深空运来。\n\n有些食材即便有钱也买不到。\n\n其中一些食材产于新星深海，有抗衰老的作用，有些食材则能大补血气，还有些食材可保人精力健旺。\n\n对于练各种散术的人来说，食补非常重要。\n\n老教授曾说，个别食材颇受新星一些身份背景惊人的老辈人物所喜，因为可以延缓衰老。\n\n可见，这次的“旧术研究”多么受重视。\n\n尤其令人吃惊的是，不久后，新星竟然来了二十几位新生加入这个实验班。\n\n从来都是旧土的学生向往新星，希冀去留学，而这次却反过来了。\n\n随着这些年轻男女的到来，陆续传出一些小道消息，让旧术实验班的学生渐渐了解到部分真相。\n\n“有些组织、有些研究机构、有些财阀，在追求长生不老！”\n\n当初这句话着实在旧术实验班中引发不小的震动，许多人都被惊住了。\n\n在现代人看来，那种追求很可笑，虚无缥缈。\n\n然而，历代以来，似乎所有走到相应高度的人都在孜孜以求。\n\n早在先秦时代，关于列仙、方士等传说，就已让后世人向往不已，连君王都不能免俗。\n\n但是，从秦皇到汉武，再到其他后来者，都失败了，求不到长生药，不可能有人成功。\n\n走到如今这个时代，所谓神话等更是早已被证伪，怎么可能存在？\n\n但是，偏偏是科技璀璨的新星传来这样的消息。\n\n“确切的说，是某些财阀中的老者，掌权的人物，想要获得更为久远的寿命。”\n\n虽然有人做了补充，但当时还是引发实验班中一片哗然。\n\n历史总是惊人的相似，历代以来，某些步入晚年的大人物似乎都很不甘心，希冀年轻，渴望有活力的身体。\n\n当年，王煊与秦诚低语，曾编排过那些大人物。\n\n“老头子们日渐衰败，自然渴求年轻，不然怎么欣赏美女？身有亿万家资，却无法再挥霍，换成是我，也想再活五百年啊！”\n\n“老太太们，绝对比老头子们更狠，韶华逝去，昔日美貌如今爬满皱纹，对于掌握大量资源、拥有强大话语权的女强人来说，老去，绝对是天敌！如果有一线可能，老太太们必然比老头子们还要疯狂，会想尽办法恢复青春！”\n\n当时，来自新星的几名年轻男女听到两人的低语后，全都猛翻白眼，因为这似乎涉及到他们各自的一些长辈。"
-    }
+        novelID: 0,
+        chapterID: 0,
+        name: '',
+        chapter: '',
+        content: ''}
 );
+
 
 
 const onClickLeft = () => history.back();
@@ -218,8 +256,10 @@ const showDefault = () => {
 };
 
 const defaultVoice = ref('');
+const defaultVoiceValue = ref('');
 
 const onConfirmDefaultVoice = ({ selectedValues }) => {
+    defaultVoiceValue.value = selectedValues[0];
     defaultVoice.value = SoundList.find(elm => elm.voice === selectedValues[0])?.name || '';
     showDefaultVoice.value = false;
 };
@@ -253,6 +293,8 @@ const AIidentify = async () => {
         showLoading.value = false;
         showRoleList.value = true;
         console.log(response.data);
+        console.log(response.data.data);
+        console.log(response.data.data.content);
         PreProcess.value = response.data.data;
         response.data.data.content.forEach((elm) => {
             highlightContent.value.push(elm.text);
@@ -285,6 +327,7 @@ const onConfirmVoice = ({ selectedValues }) => {
         if (elm.RoleID === temporaryID.value) {
             elm.voice = selectedValues[0];
             elm.voiceName = SoundList.find(elm => elm.voice === selectedValues[0])?.name || '';
+            elm.rgb = SoundList.find(elm => elm.voice === selectedValues[0])?.rgb || '';
         }
     });
     temporaryID.value = '';
@@ -295,8 +338,119 @@ const onCancelVoice = () => {
     showVoice.value = false;
 };
 
+//do process
+const doProcess = async () => {
+    try {
+        let markedText = novel.value.content;
+        //提纯markedText文本,删除其中的/n换行符
+        markedText = markedText.replace(/\n/g, '');
+        PreProcess.value.content.forEach((keyword, index) => {
+            markedText = markedText.split(keyword.text).join(`\0${keyword.text}\0`);
+        });
+        // console.log(markedText);
+        // 按标记分割文本
+        const segments = markedText.split('\0');
+
+        // 组装 JSON 对象数组
+        const result = segments.map(segment => {
+            const keywordIndex = PreProcess.value.content.findIndex(keyword => keyword.text === segment);
+            return {
+                text: segment,
+                voice: PreProcess.value.Roles.find(elm => elm.RoleID === PreProcess.value.content[keywordIndex]?.RoleID)?.voice || defaultVoiceValue.value || 'zh-CN-Xiaoxiao',
+                rgb: PreProcess.value.Roles.find(elm => elm.RoleID === PreProcess.value.content[keywordIndex]?.RoleID)?.rgb || 'default',
+            };
+        }).filter(segment => segment.text !== '');
+        //删除result中text小于5个字的元素
+        for (let i = 0; i < result.length; i++) {
+            if (result[i].text.length < 5) {
+                result.splice(i, 1);
+                i--;
+            }
+        }
+        // console.log(result);
+        // result.push({
+        //     text: '完完完完完完完完完完完完完完',
+        //     voice: 'zh-CN-Xiaoxiao',
+        //     rgb: 'default',
+        // });
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+};
+//NTS
+const doNTS = async () => {
+    showLoading.value = true;
+    await doProcess().then(async (result) => {
+        // console.log(result);
+        try {
+            const response = await axios.post('/NTS/nts', {
+                userid: 1,
+                novelID: novel.value.novelID,
+                chapterID: novel.value.chapterID,
+                novelName: novel.value.name,
+                markedNovel: result
+            });
+            showLoading.value = false;
+            console.log(response.data);
+            showFinish();
+        } catch (error) {
+            console.log(error);
+        }
+    }).catch(() => {
+        showLoading.value = false;
+    });
+};
+const showFinish = () => {
+    showConfirmDialog({
+        title: '制作完毕',
+        message:
+            '是否进行播放？',
+    })
+        .then(() => {
+            //跳转到播放页
+            router.push({
+                path: '/NTS/NTS_Play',
+                query: {
+                    novelID: novel.value.novelID,
+                    chapterID: novel.value.chapterID,
+                    novelName: novel.value.name,
+                    chapterName: novel.value.chapter,
+                }
+            });
+        })
+        .catch(() => {
+            // on cancel
+        });
+};
+
+//reset
+const reset = () => {
+    showConfirmDialog({
+        title: '重置',
+        message:
+            '是否重置？',
+    })
+        .then(() => {
+            //刷新页面
+            window.location.reload();
+        })
+        .catch(() => {
+            // on cancel
+        });
+};
 </script>
 <style scoped lang="scss">
+.pre-line {
+    white-space: pre-wrap;
+}
+.ellipsis-text {
+  width: 200px; /* 或者您希望的固定宽度 */
+  white-space: nowrap; /* 防止文本换行 */
+  overflow: hidden; /* 隐藏超出容器的内容 */
+  text-overflow: ellipsis; /* 当文本超出容器范围时显示省略号 */
+}
+
 .Menu_Container {
     height: 210px;
     margin: 10px;
--- src/views/NTS/NTS_Play.vue
+++ src/views/NTS/NTS_Play.vue
@@ -15,54 +15,251 @@
             </div>
             <!-- 点赞、收藏、评论 -->
             <div class="button-wrapper">
-                <div class="button">
+                <div class="button" @click="like()">
                     <img src="/NTS_Images/like-o.png" alt="">
-                    <p>点赞</p>
+                    <p>点赞 {{ likeCount }}</p>
                 </div>
-                <div class="button">
+                <div class="button" @click="collect()">
                     <img src="/NTS_Images/collect-o.png" alt="">
-                    <p>收藏</p>
+                    <p>收藏 {{ collectCount }}</p>
                 </div>
                 <div class="button">
                     <img src="/NTS_Images/comment.png" alt="">
-                    <p>评论</p>
+                    <p>评论 {{ commentCount }}</p>
                 </div>
             </div>
         </div>
     </div>
     <van-tabs v-model:active="active" animated>
-        <van-tab title="原文">原文</van-tab>
+        <van-tab title="原文" class="pre-line">{{ novel }}</van-tab>
         <van-tab title="评论">评论</van-tab>
     </van-tabs>
 
     <div class="bottom-placeholder"></div>
     <div class="audio-player-box">
-        <audio-player ref="audioPlayer" :audio-list="audioList.map(elm => elm.url)" :before-play="handleBeforePlay"
-            :show-prev-button="false" :show-next-button="false" theme-color="#efb336" />
+        <audio-player ref="audioPlayer" :audio-list="audioList.map(elm => elm.url)" :show-prev-button="false"
+            :show-next-button="false" theme-color="#efb336" />
     </div>
 </template>
 <script setup>
 import AudioPlayer from '@liripeng/vue-audio-player';
-import { ref } from 'vue';
+import { getCurrentInstance, ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+
+const instance = getCurrentInstance();
+const axios = instance.appContext.config.globalProperties.$axios;
+const router = useRouter();
+
+//接受参数
+let novelID = router.currentRoute.value.query.novelID;
+let chapterID = router.currentRoute.value.query.chapterID;
+let novelName = router.currentRoute.value.query.novelName;
+let chapterName = router.currentRoute.value.query.chapterName;
+let novel = ref('');
+
+//获取小说
+const getnovel = () => {
+    axios.get('/NTS/getnovel', {
+        params: {
+            novelName: novelName
+        }
+    }).then(res => {
+        console.log(res.data);
+        novel.value = res.data.data.data;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+//获取小说信息
+const likeCount = ref(0);
+const collectCount = ref(0);
+const commentCount = ref(0);
+const soundBookId = ref(0);
+const getnovelInfor = () => {
+    axios.get('/NTS/getnovelInfor', {
+        params: {
+            novelName: novelName
+        }
+    }).then(res => {
+        console.log(res.data);
+        // {
+        //     "id": 2,
+        //         "userid": 1,
+        //             "title": "[凌晨] 信使",
+        //                 "content": "SoundBooks/[凌晨] 信使_1706531092149.mp3",
+        //                     "img": null,
+        //                         "likeCount": 0,
+        //                             "collectCount": 0,
+        //                                 "commentCount": 0,
+        //                                     "viewCount": 0,
+        //                                         "createdAt": "2024-01-29T12:26:56.000Z",
+        //                                             "updatedAt": "2024-01-29T12:26:56.000Z",
+        //                                                 "usersid": null,
+        //                                                     "soundBookId": null
+        // }
+        likeCount.value = res.data.data.data.likeCount;
+        collectCount.value = res.data.data.data.collectCount;
+        commentCount.value = res.data.data.data.commentCount;
+        soundBookId.value = res.data.data.data.id;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+//findSoundBookByNovelName
+const findSoundBookByNovelName = () => {
+    axios.get('/NTS/findSoundBookByNovelName', {
+        params: {
+            novelName: novelName
+        }
+    }).then(res => {
+        console.log(res.data);
+        audioList.value[0].url = `http://127.0.0.1:3000/${res.data.data.data}`;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+//查询点赞状态
+const checkLikeStatus = () => {
+    axios.get('/NTS/getLikeStatus', {
+        params: {
+            userId: 1,
+            novelName: novelName
+        }
+    }).then(res => {
+        console.log(res.data);
+        likeCount.value = res.data.data.likeCount;
+    }).catch(err => {
+        console.log(err);
+    });
+}
+//onMounted
+onMounted(() => {
+    getnovel();
+    getnovelInfor();
+    findSoundBookByNovelName();
+});
 
 const onClickLeft = () => history.back();
 
-const currentAudioName = ref('默认有声书播放有声小说播放有声小说播放有声小说播放有声小说');
-const currentAudioChapter = ref('默认有声书播放有声小说播放有声小说播放有声小说播放有声小说');
+const currentAudioName = ref(novelName);
+const currentAudioChapter = ref(chapterName);
 
 const audioList = ref([
     {
         name: 'audio1',
-        url: 'https://www.0dutv.com/upload/dance/F25F74A0B8FF82503241801D0E2CA5CD.mp3'
-    }
+        url: ''
+    },
 ]);
-const handleBeforePlay = (index) => {
-    currentAudioName.value = audioList.value[index].name;
-};
+
 
 const active = ref(0);
+//like
+const like = () => {
+    //查询点赞状态
+    axios.get('/NTS/getLikeStatus', {
+        params: {
+            userId: 1,
+            novelName: novelName
+        }
+    }).then(res => {
+        console.log(res.data);
+        console.log(res.data.data.isLike);
+        likeCount.value = res.data.data.likeCount;
+        console.log(soundBookId.value);
+        if (res.data.data.isLike == false) {
+            //未点赞
+            console.log('未点赞dh');
+            console.log(novelName)
+            axios.post('/NTS/likeSoundBook', {
+                userId: 1,
+                novelName: novelName,
+                likeCount: likeCount.value,
+                soundBookId: soundBookId.value
+            }).then(res => {
+                console.log(likeCount.value);
+                likeCount.value+=1;
+                console.log(likeCount.value);
+            }).catch(err => {
+                console.log(err);
+            });
+        } else {
+            //已点赞
+            console.log('已点赞dh');
+            axios.post('/NTS/dislikeSoundBook', {
+                userId: 1,
+                novelName: novelName,
+                likeCount: likeCount.value,
+                soundBookId: soundBookId.value
+            }).then(res => {
+                console.log(likeCount.value);
+                likeCount.value-=1;
+                console.log(likeCount.value);
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+}
+//collect
+const collect = () => {
+    //查询点赞状态
+    axios.get('/NTS/getCollectStatus', {
+        params: {
+            userId: 1,
+            novelName: novelName
+        }
+    }).then(res => {
+        console.log(res.data);
+        console.log(res.data.data.isCollect);
+        collectCount.value = res.data.data.collectCount;
+        console.log(soundBookId.value);
+        if (res.data.data.isCollect == false) {
+            //未点赞
+            console.log('未收藏dh');
+            console.log(novelName)
+            axios.post('/NTS/collectSoundBook', {
+                userId: 1,
+                novelName: novelName,
+                collectCount: collectCount.value,
+                soundBookId: soundBookId.value
+            }).then(res => {
+                console.log(collectCount.value);
+                collectCount.value+=1;
+                console.log(collectCount.value);
+            }).catch(err => {
+                console.log(err);
+            });
+        } else {
+            //已点赞
+            console.log('已收藏dh');
+            axios.post('/NTS/uncollectSoundBook', {
+                userId: 1,
+                novelName: novelName,
+                collectCount: collectCount.value,
+                soundBookId: soundBookId.value
+            }).then(res => {
+                console.log(collectCount.value);
+                collectCount.value-=1;
+                console.log(collectCount.value);
+            }).catch(err => {
+                console.log(err);
+            });
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
 </script>
 <style scoped lang="scss">
+.pre-line {
+    white-space: pre-wrap;
+    padding: 10px;
+    padding-bottom: 40px;
+}
+
 .infor-box {
     display: flex;
     margin-top: 10px;
@@ -97,6 +294,7 @@ const active = ref(0);
         }
 
         .button-wrapper {
+            padding: 0 20px;
             display: flex;
             justify-content: space-between;
 
